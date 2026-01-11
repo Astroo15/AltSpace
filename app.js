0cddaf6ad7e9b3fb9aa0374cd51f9b7cef761ba8
Astroo15 <angelhodges2013@gmail.com>
2026-01-11 20:37:54 +0100

    fix auth redirect and app.js structure

--- app.js
+++ app.js
@@ -1,59 +1,47 @@
+// DEBUG: confirm file loads
+console.log("app.js loaded");
+
+// Firebase core
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+
+// Firebase auth
 import {
   getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword
+  onAuthStateChanged,
+  signOut
 } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
 
+// Your Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyDNLeNACWjr0dr41g_utjpVBdGUpTwCJdg",
   authDomain: "altspace-25aed.firebaseapp.com",
   projectId: "altspace-25aed",
   storageBucket: "altspace-25aed.firebasestorage.app",
   messagingSenderId: "671097190540",
-  appId: "1:671097190540:web:5cb724024e1ffd41d963a9"
+  appId: "1:671097190540:web:5cb724024e1ffd41d963a9",
+  measurementId: "G-LSD853EXQV"
 };
 
+// Init Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-window.signup = function () {
-  createUserWithEmailAndPassword(
-    auth,
-    email.value,
-    password.value
-  ).then(() => {
-    alert("Account created 🖤");
-    location.href = "astro.html";
-  }).catch(e => alert(e.message));
-};
-
-window.login = function () {
-  signInWithEmailAndPassword(
-    auth,
-    email.value,
-    password.value
-  ).then(() => {
-    alert("Logged in 💿");
-    location.href = "astro.html";
-  }).catch(e => alert(e.message));
-};
-import { getAuth, onAuthStateChanged } from 
-"https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
-
-const auth = getAuth(app);
-
+// Auth state listener
 onAuthStateChanged(auth, (user) => {
+  console.log("auth state changed:", user);
+
   if (user) {
-    window.location.href = "home.html";
+    console.log("redirecting to home.html");
+    if (!window.location.pathname.endsWith("home.html")) {
+      window.location.href = "home.html";
+    }
   }
 });
 
-    import { signOut } from 
-"https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
-
+// Logout function
 window.logout = function () {
   signOut(auth).then(() => {
     window.location.href = "index.html";
   });
 };
+

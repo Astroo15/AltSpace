7b281fc75cff326a248beca03e8a8b8e5ade172c
Astroo15 <angelhodges2013@gmail.com>
2026-01-11 13:49:25 +0100

    replace js test log with firebase initialization

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-console.log("AltSpace JS connected 🕸️");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
+console.log("🔥 Firebase connected and ready");
